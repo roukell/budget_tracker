9de7f171b5de21688dfaf338e8f49756cdbfc2e9
roukell <62136980+roukell@users.noreply.github.com>
2020-09-19 00:35:10 +1000

    update PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
